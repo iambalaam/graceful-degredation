cf6f506deb85002d4093444301d2d32cc504053c
Guy Balaam <guybal@amazon.com>
2019-02-26 15:46:35 +0000

    Initial degredation from <a> and <form>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
+
+const Info = () => <h2>🔎</h2>
+const About = () => <h2>📚</h2>
+const Home = () => <h2>🏡</h2>
+
+const gracefullyDegrade = (e:
+  React.MouseEvent<HTMLAnchorElement> |
+  React.FormEvent<HTMLFormElement>) => {
+  const target = e.nativeEvent.target;
+
+  if (target instanceof HTMLAnchorElement && target.href) {
+    window.location.href = target.href;
+  }
+
+  if (target instanceof HTMLFormElement && target.method === 'get') {
+    let query: string[] = [];
+    const iterator = new FormData(target).entries();
+    while (true) {
+      const { done, value } = iterator.next();
+      if (done) break;
+      const [key, val] = value;
+      query.push(`${key}=${val}`);
+    }
+    window.location.search = '?' + query.join('&');
+  }
+}
+
+const handleAndFail = (e:
+  React.MouseEvent<HTMLAnchorElement> |
+  React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  try {
+    throw new Error("Couldn't do the thing on the webpage");
+  } catch (err) {
+    gracefullyDegrade(e);
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <header>
+              <NavLink to='/' onClick={handleAndFail}>🏡</NavLink>
+              <NavLink to='/info'>🔎</NavLink>
+              <NavLink to='/about'>📚</NavLink>
+            </header>
+            <Switch>
+              <Route path='/info' component={Info} />
+              <Route path='/about' component={About} />
+              <Route component={Home} />
+            </Switch>
+            <form method="GET" onSubmit={handleAndFail} >
+              <label> Label:
+                <input name="form-submit" type="text" />
+              </label>
+              <button type="submit">submit</button>
+            </form>
+          </div>
+        </Router>
       </div>
     );
   }
